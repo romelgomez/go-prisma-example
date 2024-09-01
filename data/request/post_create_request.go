02712469d74c1861bcddb8bf257d103d0542a0fc
romelgomez <bmxandcode@gmail.com>
2024-08-31 22:51:20 -0500

    feat: add part of the service

---
+++ data/request/post_create_request.go
@@ -0,0 +1,7 @@
+package request
+
+type PostCreateRequest struct {
+	Title       string `validate:"required min=1,max=100" json:"title"`
+	Published   bool   `json:"published"`
+	Description string `json:"description"`
+}
